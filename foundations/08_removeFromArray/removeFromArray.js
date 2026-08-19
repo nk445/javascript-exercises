cbf2278e79860876ef2d41d36478bc2b0d6a411a
Nahom Kibreab <143555609+nk445@users.noreply.github.com>
2026-08-19 17:23:43 -0400

    Complete foundations exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    let toRemove = Array.from(arguments).slice(1);
+    let newArr = arr.filter(item => !toRemove.includes(item))
+    return newArr;
 };
 
 // Do not edit below this line
