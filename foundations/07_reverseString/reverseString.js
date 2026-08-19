17d0a36e6ac3d316cd9fbf4ad393b3beddb5c9b2
Nahom Kibreab <143555609+nk445@users.noreply.github.com>
2026-08-18 21:18:14 -0400

    Complete foundations exercise 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = [];
+    for (let i = 0; i < str.length; ++i) {
+        arr.unshift(str[i]);
+    }
+    return arr.join("");
 };
 
 // Do not edit below this line
